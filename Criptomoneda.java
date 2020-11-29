e76bdea08d7f6fee59854930084901e36d2e7290
Agustin Bergomi <agustin.bergomi@gmail.com>
2020-11-29 12:19:08 -0300

    Create Criptomoneda.java

---
+++ Criptomoneda.java
@@ -0,0 +1,14 @@
+//clase
+public class Criptomoneda{
+
+//atributos
+private String nombre;
+private String sigla
+private float valor;
+private float variacion;
+
+//metodos
+getters y setters
+
+
+}
